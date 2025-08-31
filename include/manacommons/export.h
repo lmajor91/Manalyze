59e3fd837f3fbb0f3f1e9797c467b37014e667e7
Liam Major <liam.major@gmail.com>
2025-08-31 15:13:59 -0600

    feat: added a centralized file to epxorting as a library

---
+++ include/manacommons/export.h
@@ -0,0 +1,33 @@
+/*
+This file is part of Manalyze.
+
+Manalyze is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Manalyze is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Manalyze.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+// this just contains export logic for DLLs and SOs
+
+#ifndef __MANACOMMONS_EXPORT__
+#define __MANACOMMONS_EXPORT__ 1
+
+#if defined BOOST_WINDOWS_API && !defined DECLSPEC_MANACOMMONS
+#ifdef MANACOMMONS_EXPORT
+#define DECLSPEC_MANACOMMONS __declspec(dllexport)
+#else
+#define DECLSPEC_MANACOMMONS __declspec(dllimport)
+#endif
+#elif !defined BOOST_WINDOWS_API && !defined DECLSPEC_MANACOMMONS
+#define DECLSPEC_MANACOMMONS
+#endif
+
+#endif // __MANACOMMONS_EXPORT__
